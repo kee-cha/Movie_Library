572919b0ac78d7f73752bc62e76800b62a9ccfc0
Michael Gonzalez Jr <mikegjr95@gmail.com>
2020-01-14 08:39:45 -0600

    fixed post method

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -43,25 +43,7 @@ namespace WebAPISample.Controllers
         public IHttpActionResult Get(int id)
         {
             // Retrieve movie by id from db logic
-<<<<<<< HEAD
-            IList<Movies> movies = null;
-            
-            using (var mList = new ApplicationDbContext())
-            {
-                movies = mList.Movies.Include("Title")
-                    .Select(m => new Movies()
-                    {
-                        Title = m.Title,
-                        Genre = m.Genre,
-                        DirectorName = m.DirectorName
-                    }).ToList<Movies>();
-            }
-            if(movies.Count == 0)
-            {
-                return NotFound();
-            }
-            return Ok();
-=======
+
             var movie = context.Movies.Where(m => m.MovieId == id).SingleOrDefault();
             if (movie == null)
             {
@@ -69,7 +51,7 @@ namespace WebAPISample.Controllers
             }
             
             return Ok(movie);
->>>>>>> 614a10ff1dc2f3f3bf3d32be6a497690b4c4be51
+
         }
 
         // POST api/values
@@ -82,13 +64,11 @@ namespace WebAPISample.Controllers
             }
             using (var context = new ApplicationDbContext())
             {
-                context.Movies.Add(new Movies()
-                {
-                    Title = value.Title,
-                    Genre = value.Genre,
-                    DirectorName = value.DirectorName
-                });
-
+                Movies movie = new Movies();
+                movie.Title = value.Title;
+                movie.Genre = value.Genre;
+                movie.DirectorName = value.DirectorName;
+                context.Movies.Add(movie);
                 context.SaveChanges();
             }
             return Ok();
@@ -121,7 +101,6 @@ namespace WebAPISample.Controllers
         }
 
         // DELETE api/values/5
-<<<<<<< HEAD
         public void Delete(int id, [FromBody]Movies value)
         {
             // Delete movie from db logic
@@ -129,22 +108,7 @@ namespace WebAPISample.Controllers
             var movie = context.Movies.Where(m => m.MovieId == id).SingleOrDefault();
             context.Movies.Remove(movie);
             context.SaveChanges();
-=======
-        public IHttpActionResult Delete(int id)
-        {
-            // Delete movie from db logic
-            if (id <= 0)
-            {
-                return BadRequest("Not a valid id");
-            }
-            using (var context = new ApplicationDbContext())
-            {
-                var movie = context.Movies.Where(m => m.MovieId == id).SingleOrDefault();
-                context.Entry(movie).State = System.Data.Entity.EntityState.Deleted;
-                context.SaveChanges();
-            }
-            return Ok();
->>>>>>> 614a10ff1dc2f3f3bf3d32be6a497690b4c4be51
+
         }
     }
 
