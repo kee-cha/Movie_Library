ca093e50630b6bbaf710e247c725c4f4803ccc07
Michael Gonzalez Jr <mikegjr95@gmail.com>
2020-01-15 10:21:33 -0600

    working on put will have to revisit Post, stopped functioning correctly

--- Client/app1.js
+++ Client/app1.js
@@ -1,7 +1,6 @@
-(function ($)
-{
+(function ($) {
     var dataTable = $('#movieTable');
-    function processForm(e){
+    function processForm(e) {
         var dict = {
             Title: this["title"].value,
             Genre: this["genre"].value,
@@ -10,10 +9,10 @@
         $.ajax({
             url: 'https://localhost:44352/api/Movie',
             dataType: 'json',
-            method: 'Post',
+            type: 'Post',
             contentType: 'application/json',
-            data: JSON.stringify(dict,data),
-            success: function (dict,data, textStatus, jQxhr) {
+            data: JSON.stringify(dict),
+            success: function (data, textStatus, jQxhr) {
                 dataTable.append('<tr><td>' + dict.Title + '</td><td>' + dict.Genre + '</td><td>' + dict.DirectorName + '</td></tr>')
             },
             error: function (jqXhr, textStatus, errorThrown) {
@@ -22,42 +21,29 @@
         });
         e.preventDefault();
     }
-    
     $('#add-movie').submit(processForm);
-
-    function getTableValue(){       
+    function newTable() {
+        
+        getTableValue(function (i, value) {
+            if (i === 0) {
+            } else {
+                dataTable.append('<tr><td>' + value.Title + '</td><td>' + value.Genre + '</td><td>' + value.DirectorName + '</td></tr>')
+            }
+        });
+    }
+    function getTableValue(method) {
         $.ajax({
             url: 'https://localhost:44352/api/Movie',
             dataType: 'json',
-            method: 'Get',
+            type: 'Get',
             contentType: 'application/json',
             success: function (data, textStatus, jqXhr) {
-                $.each(data, function (i, value) {
-                dataTable.append('<tr><td>' + value.Title + '</td><td>' + value.Genre + '</td><td>' + value.DirectorName + '</td></tr>')
-            });
+                $.each(data, method);
             },
             error: function (jqXhr, textStatus, errorThrown) {
                 console.log(errorThrown);
             }
         });
     }
-    document.getElementById("table").innerHTML = getTableValue();
-
-
-
-    function updateTableValue(){
-        $.ajax({
-            url: 'https://localhost:44352/api/Movie',
-            dataType: 'json',
-            method: 'Put',
-            contentType: 'application/json',
-            data: JSON.stringify(dict), 
-            success: function (data) {
-
-            }
-
-        });    
-    }
+    document.getElementById("table").innerHTML = newTable();
 })(jQuery);
-
-
